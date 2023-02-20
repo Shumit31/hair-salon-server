eeffb17192c77c3962d7beecf81a9c6b066209ef
1998MahmudurRahman <rahmanshumit1998@gmail.com>
2023-02-20 22:04:54 +0600

    Payment server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app=express();
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const jwt  = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 5000;
@@ -51,6 +52,7 @@ async function run(){
     const bookingsCollection =client.db('hairSalon').collection('bookings');
     const usersCollection =client.db('hairSalon').collection('users');
     const specialistsCollection =client.db('hairSalon').collection('specialists');
+    const paymentsCollection =client.db('hairSalon').collection('payments');
 
 
       // NOTE: make sure you use verifyAdmin after verifyJWT
@@ -155,6 +157,40 @@ app.get('/bookings/:id', async(req,res)=>{
     const result = await bookingsCollection.insertOne(booking);
     res.send(result);
    });
+
+   app.post('/create-payment-intent', async (req, res) => {
+    const booking = req.body;
+    const price = booking.price;
+    const amount = price * 100;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: amount,
+        "payment_method_types": [
+            "card"
+        ]
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+
+app.post('/payments', async (req, res) =>{
+  const payment = req.body;
+  const result = await paymentsCollection.insertOne(payment);
+  const id = payment.bookingId
+  const filter = {_id: new ObjectId(id)}
+  const updatedDoc = {
+      $set: {
+          paid: true,
+          transactionId: payment.transactionId
+      }
+  }
+  const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+  res.send(result);
+})
+
 
 
 
