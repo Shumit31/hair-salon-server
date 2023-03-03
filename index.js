cff432065dd66af2f03960434c58a0d16872637d
1998MahmudurRahman <rahmanshumit1998@gmail.com>
2023-03-04 02:47:49 +0600

    add comment

--- index.js
+++ index.js
@@ -8,23 +8,17 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 5000;
 
 
-
-
-
-
-
-
+//middleware
 app.use(cors());
 app.use(express.json());
 
 
-
-
-
+//mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qcvnypp.mongodb.net/?retryWrites=true&w=majority`;
 
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 function verifyJWT(req,res,next){
@@ -44,7 +38,7 @@ function verifyJWT(req,res,next){
  })
 }
 
-
+//find multiple documents
 async function run(){
  try{
     const appointmentsectionCollection = client.db('hairSalon').collection('appointmentsection');
@@ -53,6 +47,9 @@ async function run(){
     const usersCollection =client.db('hairSalon').collection('users');
     const specialistsCollection =client.db('hairSalon').collection('specialists');
     const paymentsCollection =client.db('hairSalon').collection('payments');
+
+
+
 
 
       // NOTE: make sure you use verifyAdmin after verifyJWT
@@ -70,24 +67,26 @@ async function run(){
     
 
    //use aggrigate to query multiple collection and merge data
+
+   //appointsection service server code
     app.get('/appointmentsection',async(req,res)=>{
       const date=req.query.date;
-        const query ={};
+        const query ={};//to get all the data
         const sections = await appointmentsectionCollection.find(query).toArray();
-
+        
         //get the bookings of the provided date
         const bookingQuery={appointmentDate:date
         }
         const alreadyBooked=await bookingsCollection.find(bookingQuery).toArray();
-
-        
-
-        //code carefully
+  
+        //to get  available alreadybooked bookings
         sections.forEach(section=>{
           const sectionBooked=alreadyBooked.filter(book=>book.service=== section.name);
+
           const bookedSlots = sectionBooked.map(book=>book.slot);
          
-          const remainingSlots=section.slots.filter(slot=>!bookedSlots.includes(slot));
+          //remailing slots
+          const remainingSlots=section.slots.filter(slot=>!bookedSlots.includes(slot));  //give the data which sots are not booked availaable 
           section.slots=remainingSlots;
           console.log(date,section.name,remainingSlots.length);
           
@@ -99,24 +98,47 @@ async function run(){
 
 
 
+
+
+
+    // to get the exact name field from the  specialist section
  app.get('/appointmentSpeciality' ,async (req,res)=>{
   const query ={}
   const result = await appointmentsectionCollection.find(query).project({name:1}).toArray();
   res.send(result);
  })
 
+ //to get the specialist data
+app.get('/specialists',verifyJWT, verifyAdmin, async(req,res)=>{
+  const query = {};
+  const specialist = await specialistsCollection.find(query).toArray();
+  res.send(specialist);
+
+})
+
+app.post('/specialists', verifyJWT,verifyAdmin, async (req,res)=>{
+  const specialist = req.body;
+  const result = await specialistsCollection.insertOne(specialist);
+  res.send(result);
+  
+ 
+ 
+})
+
 
   /***
    * ApI naming convention
    
    * bookings
-   * app.get('/bookings/')
-   * app.get('/bookings/:id')
-   * app.post('/bookings')
-   * app.patch('bookings/:id')
-   * app.delete('/bookings/:id')
+   * app.get('/bookings/') all bookings to get
+   * app.get('/bookings/:id') specifice id booking
+   * app.post('/bookings')   new booking new object
+   * app.patch('bookings/:id') update specific id
+   * app.delete('/bookings/:id') delete specific delete
    */
 
+
+  //to get the my appointment code through email field
   app.get('/bookings', verifyJWT, async (req, res) => {
     const email = req.query.email;
     const decodedEmail = req.decoded.email;
@@ -130,6 +152,28 @@ async function run(){
     res.send(bookings);
 })
 
+
+//modal to submit booking and give one booking at a time
+
+app.post('/bookings',async(req,res)=>{
+  const booking= req.body
+  const query ={
+    appointmentDate:booking.appointmentDate,
+    email:booking.email,
+    service: booking.service
+  }
+
+const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+if(alreadyBooked.length){
+  const message= `You already have a appointment on ${booking.appointmentDate}`
+  return res.send({acknowledged:false,message})
+}
+
+  const result = await bookingsCollection.insertOne(booking);
+  res.send(result);
+ });
+
 app.get('/bookings/:id', async(req,res)=>{
   const id = req.params.id;
   
@@ -139,24 +183,12 @@ app.get('/bookings/:id', async(req,res)=>{
 
 })
 
-   app.post('/bookings',async(req,res)=>{
-    const booking= req.body
-    const query ={
-      appointmentDate:booking.appointmentDate,
-      email:booking.email,
-      service: booking.service
-    }
 
-  const alreadyBooked = await bookingsCollection.find(query).toArray();
+   
 
-  if(alreadyBooked.length){
-    const message= `You already have a appointment on ${booking.appointmentDate}`
-    return res.send({acknowledged:false,message})
-  }
 
-    const result = await bookingsCollection.insertOne(booking);
-    res.send(result);
-   });
+
+   
 
    app.post('/create-payment-intent', async (req, res) => {
     const booking = req.body;
@@ -193,7 +225,7 @@ app.post('/payments', async (req, res) =>{
 
 
 
-
+  //to generate  token
    app.get('/jwt', async (req, res) => {
     const email = req.query.email;
     const query = { email: email };
@@ -205,6 +237,8 @@ app.post('/payments', async (req, res) =>{
     res.status(403).send({ accessToken: '' })
 });
 
+
+//to load all the users
 app.get('/users',async(req,res)=>{
   const query = {};
   const users = await usersCollection.find(query).toArray();
@@ -212,7 +246,7 @@ app.get('/users',async(req,res)=>{
 });
 
 
-
+// to check if this user is admin or not
 app.get('/users/admin/:email', async(req,res)=>{
   const email = req.params.email;
   const query ={email}
@@ -226,6 +260,7 @@ app.get('/users/admin/:email', async(req,res)=>{
 
 
 
+   //to get the information of the user
 
    app.post('/users', async (req, res) => {
     const user = req.body;
@@ -235,6 +270,8 @@ app.get('/users/admin/:email', async(req,res)=>{
 });
 
 
+
+//to make admin and update it
 app.put('/users/admin/:id',verifyJWT,verifyAdmin, async(req,res)=>{
 
 
@@ -267,27 +304,23 @@ app.put('/users/admin/:id',verifyJWT,verifyAdmin, async(req,res)=>{
 
 // })
 
-app.get('/specialists',verifyJWT, verifyAdmin, async(req,res)=>{
-  const query = {};
-  const specialist = await specialistsCollection.find(query).toArray();
-  res.send(specialist);
 
-})
 
-app.post('/specialists', verifyJWT,verifyAdmin, async (req,res)=>{
-  const specialist = req.body;
-  const result = await specialistsCollection.insertOne(specialist);
-  res.send(result);
-  
- 
- 
-})
+
 app.delete('/specialists/:id',verifyJWT,verifyAdmin, async(req,res)=>{
   const id = req.params.id;
   const filter = {_id:new ObjectId(id)};
   const result = await specialistsCollection.deleteOne(filter);
   res.send(result);
    })
+
+   
+   app.delete('/users/:id',verifyJWT,verifyAdmin, async(req,res)=>{
+    const id = req.params.id;
+    const filter = {_id:new ObjectId(id)};
+    const result = await usersCollection.deleteOne(filter);
+    res.send(result);
+     })
  
 
  }
